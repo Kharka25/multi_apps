016d72574a7bbeb2dba7e1745a3f59b8786b5c3e
Kharka25 <adewoyindaniel6@gmail.com>
2025-07-06 17:33:28 +0100

    setup access screen

--- app/src/main/java/com/example/multiapps/MainActivity.java
+++ app/src/main/java/com/example/multiapps/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.multiapps;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Button signinButton;
+    EditText nameInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        signinButton = findViewById(R.id.signinButton);
+        nameInput = findViewById(R.id.userNameInput);
+
+        signinButton.setOnClickListener(v -> {
+            if (nameInput.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show();
+                return;
+            }
+            signinIn(nameInput.getText().toString());
+        });
+    }
+
+    public void signinIn(String userName) {
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.putExtra("username", userName);
+        startActivity(intent);
     }
 }
